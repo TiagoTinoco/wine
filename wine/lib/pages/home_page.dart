0bae460ccd8c14f6d762c9da883977f017ce1945
Tiago Tinoco <tiagotinocomartinsdossantos@gmail.com>
2022-04-28 17:24:44 -0300

    Fix errors and add new actions

--- wine/lib/pages/home_page.dart
+++ wine/lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:wine/models/wine.dart';
+import 'package:wine/pages/cart_page.dart';
+import 'package:wine/pages/form_page.dart';
 import 'package:wine/pages/wines_card_page.dart';
 import 'package:wine/repository/wine.dart';
+import 'package:wine/widgets/logo.dart';
 import 'package:wine/widgets/wine_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,7 +17,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final wineRepository = WineRepository();
-  late final winesFuture = wineRepository.getWine();
+  late Future<List<Wine>> winesFuture = wineRepository.getWines();
 
   @override
   Widget build(BuildContext context) {
@@ -22,32 +25,34 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 2,
-        title: RichText(
-          text: TextSpan(
-            text: 'Wi',
-            style: TextStyle(
-              fontSize: 50,
-              color: Colors.pink[200],
-              fontWeight: FontWeight.bold,
-              fontFamily: 'PlayfairDisplay',
-            ),
-            children: const <TextSpan>[
-              TextSpan(
-                text: ' ne.',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                  color: Color.fromARGB(255, 45, 40, 122),
-                ),
-              ),
-            ],
-          ),
-        ),
+        title: const LogoWidget(),
         actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            color: const Color.fromARGB(255, 45, 40, 122),
+            onPressed: () async {
+              bool value = await Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => const FormWinePage()),
+              );
+
+              if (value == true) {
+                setState(() {
+                  winesFuture = wineRepository.getWines();
+                });
+              }
+            },
+          ),
+          const SizedBox(width: 10),
           IconButton(
             icon: const Icon(Icons.shopping_cart_outlined),
             color: const Color.fromARGB(255, 45, 40, 122),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const CartPage(),
+                ),
+              );
+            },
           ),
           const SizedBox(width: 20),
           CircleAvatar(backgroundColor: Colors.pink[200]),
@@ -75,20 +80,31 @@ class _HomePageState extends State<HomePage> {
                   return WineCard(
                     wine: wine.name,
                     price: wine.price,
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (_) => WinescardPage(
-                            name: wine.name,
-                            temperature: wine.temperature.toInt(),
-                            grape: wine.grapes,
-                            pairings: wine.pairings,
-                            price: wine.price,
-                            qtde: wine.qtde,
-                          ),
-                        ),
-                      );
-                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(13),
+                        border: Border.all(color: Colors.white, width: 1, style: BorderStyle.solid),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_forward_ios_rounded),
+                        color: Colors.white,
+                        iconSize: 20,
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (_) => WinescardPage(
+                                name: wine.name,
+                                temperature: wine.temperature.toInt(),
+                                grape: wine.grapes,
+                                pairings: wine.pairings,
+                                price: wine.price,
+                                id: wine.id!.toInt(),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
                   );
                 },
               );
